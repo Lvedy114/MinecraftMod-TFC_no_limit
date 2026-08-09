67e1a71fa5007bbd756691c380ab4252cd90875f
lvedy <2063622031@qq.com>
2026-08-09 15:35:11 +0800

    Fix logic issue where mixin didn't actually set custom max stack size properly

--- src/main/java/com/lvedy/tfc_no_limit/mixin/TFCComponentsMixin.java
+++ src/main/java/com/lvedy/tfc_no_limit/mixin/TFCComponentsMixin.java
@@ -1,17 +1,68 @@
 package com.lvedy.tfc_no_limit.mixin;
 
-import com.lvedy.tfc_no_limit.TfcNoLimit;
+import com.lvedy.tfc_no_limit.Config;
 import net.dries007.tfc.common.component.TFCComponents;
+import net.dries007.tfc.common.component.size.ItemSizeManager;
+import net.dries007.tfc.common.component.size.Weight;
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(TFCComponents.class)
 public class TFCComponentsMixin {
 
-    @Inject(method = "onModifyDefaultComponentsAfterResourceReload", at = @At("TAIL"), remap = false)
-    private static void injectCustomStackSizes(CallbackInfo ci) {
-        TfcNoLimit.applyCustomStackSizes();
+    @Inject(
+            method = "onModifyDefaultComponentsAfterResourceReload",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/core/component/DataComponentPatch$Builder;build()Lnet/minecraft/core/component/DataComponentPatch;",
+                    shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            remap = false
+    )
+    private static void injectCustomStackSizes(
+            CallbackInfo ci,
+            boolean flag,
+            int modifiedCount,
+            net.minecraft.world.food.FoodProperties.Builder foodBuilder,
+            net.minecraft.world.food.FoodProperties defaultFood,
+            java.util.Iterator<?> iterator,
+            Item item,
+            ItemStack stack,
+            boolean hasFood,
+            net.dries007.tfc.common.component.food.FoodDefinition foodDef,
+            boolean setFood,
+            int currentMaxStackSize,
+            int tfcStackSize,
+            DataComponentPatch.Builder patchBuilder
+    ) {
+        if (!Config.ENABLE_CUSTOM_STACK_SIZES.get()) return;
+
+        Weight weight = ItemSizeManager.get(stack).getWeight(stack);
+
+        int newStackSize = 64;
+        if (weight == Weight.VERY_LIGHT) {
+            newStackSize = Config.VERY_LIGHT_STACK_SIZE.get();
+        } else if (weight == Weight.LIGHT) {
+            newStackSize = Config.LIGHT_STACK_SIZE.get();
+        } else if (weight == Weight.MEDIUM) {
+            newStackSize = Config.MEDIUM_STACK_SIZE.get();
+        } else if (weight == Weight.HEAVY) {
+            newStackSize = Config.HEAVY_STACK_SIZE.get();
+        } else if (weight == Weight.VERY_HEAVY) {
+            newStackSize = Config.VERY_HEAVY_STACK_SIZE.get();
+        }
+
+        if (currentMaxStackSize != newStackSize) {
+            patchBuilder.set(DataComponents.MAX_STACK_SIZE, newStackSize);
+        }
     }
 }
+
