c475ffc23d3f828c2e409a0c33626723cf8ded26
lvedy <2063622031@qq.com>
2026-08-09 16:37:11 +0800

    重写群峦原版最大堆叠限制

--- src/main/java/com/lvedy/tfc_no_limit/Config.java
+++ src/main/java/com/lvedy/tfc_no_limit/Config.java
@@ -1,9 +1,15 @@
 package com.lvedy.tfc_no_limit;
 
+import net.dries007.tfc.common.component.size.Weight;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    private static final int VANILLA_VERY_LIGHT = 64;
+    private static final int VANILLA_LIGHT = 32;
+    private static final int VANILLA_MEDIUM = 16;
+    private static final int VANILLA_HEAVY = 4;
+    private static final int VANILLA_VERY_HEAVY = 1;
 
     public static final ModConfigSpec.BooleanValue ENABLE_CUSTOM_STACK_SIZES = BUILDER
             .comment("Enable custom max stack sizes based on TFC weight.")
@@ -30,4 +36,31 @@ public class Config {
             .defineInRange("veryHeavyStackSize", 64, 1, 99);
 
     static final ModConfigSpec SPEC = BUILDER.build();
+
+    private Config() {
+    }
+
+    public static int getStackSize(Weight weight) {
+        if (!ENABLE_CUSTOM_STACK_SIZES.get()) {
+            return getVanillaStackSize(weight);
+        }
+
+        return switch (weight) {
+            case VERY_LIGHT -> VERY_LIGHT_STACK_SIZE.get();
+            case LIGHT -> LIGHT_STACK_SIZE.get();
+            case MEDIUM -> MEDIUM_STACK_SIZE.get();
+            case HEAVY -> HEAVY_STACK_SIZE.get();
+            case VERY_HEAVY -> VERY_HEAVY_STACK_SIZE.get();
+        };
+    }
+
+    private static int getVanillaStackSize(Weight weight) {
+        return switch (weight) {
+            case VERY_LIGHT -> VANILLA_VERY_LIGHT;
+            case LIGHT -> VANILLA_LIGHT;
+            case MEDIUM -> VANILLA_MEDIUM;
+            case HEAVY -> VANILLA_HEAVY;
+            case VERY_HEAVY -> VANILLA_VERY_HEAVY;
+        };
+    }
 }
--- src/main/java/com/lvedy/tfc_no_limit/TfcNoLimitClient.java
+++ src/main/java/com/lvedy/tfc_no_limit/TfcNoLimitClient.java
@@ -1,6 +1,5 @@
 package com.lvedy.tfc_no_limit;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -10,22 +9,15 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-// This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = TfcNoLimit.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = TfcNoLimit.MODID, value = Dist.CLIENT)
 public class TfcNoLimitClient {
     public TfcNoLimitClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        TfcNoLimit.LOGGER.info("HELLO FROM CLIENT SETUP");
-        TfcNoLimit.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        TfcNoLimit.LOGGER.debug("Registered TFC No Limit client config screen.");
     }
 }
